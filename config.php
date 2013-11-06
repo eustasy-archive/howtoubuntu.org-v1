db4d49374ee0182faba2c9190e9cf4deee4995ca
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-11-06 14:01:13 +0000

    Cleared config.php for public viewing

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+	// This is the Title of your Site
+	$Sitewide_Title = 'How to Ubuntu';
+
+	// This is the Base URL of your Site
+	$Sitewide_Root = 'http://b.howtoubuntu.org';
+	// Don't include a trailing slash
+	// Do include a http:// or https://
+
+?>
