4e3edcd5d014168384fea0af4389319dbda58b0e
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-11-21 14:05:50 +0000

    Integrate Config into Request

--- request.php
+++ request.php
@@ -1,6 +1,12 @@
 <?
 
-	require 'config.php';
+	// This is the Title of your Site
+	$Sitewide_Title = 'How to Ubuntu';
+
+	// This is the Base URL of your Site
+	$Sitewide_Root = 'http://b.howtoubuntu.org';
+	// Don't include a trailing slash
+	// Do include a http:// or https://
 
 	$Request = parse_url($Sitewide_Root.$_SERVER['REQUEST_URI']);
 
