fafeb667c3437a2ad80e177cc0a04a19a7e5e844
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2013-11-23 18:31:34 +0000

    Fixed Blockquote with nested Paragraph Tag

--- broadcast/how-to-install-spotify-in-ubuntu.php
+++ broadcast/how-to-install-spotify-in-ubuntu.php
@@ -18,7 +18,9 @@ if (htmlentities($Request['path'], ENT_QUOTES, 'UTF-8') == '/' . $Canonical) {
 		<div class="col span_4_of_6">
 			<h2>How to Install Spotify in Ubuntu</h2>
 			<p>While Spotify has no supported release for Linux as such, their is, hidden away a little bit, a nice little "Preview" release that runs reasonably well, even if it is not the most native of applications.</p>
-			<blockquote>This is a preview build of Spotify for Linux. As a preview release this version is still unsupported, but we're running it ourselves and will try to make sure it keeps pace with its Mac and Windows siblings.</blockquote>
+			<blockquote>
+				<p>This is a preview build of Spotify for Linux. As a preview release this version is still unsupported, but we're running it ourselves and will try to make sure it keeps pace with its Mac and Windows siblings.</p>
+			</blockquote>
 			<p><a href="https://www.spotify.com/">Spotify</a> is a great way to listen to music by streaming it on your phone, in your browser, or on your Linux desktop. You can stream everything, upgrade and sync tracks and playlists offline, or purchase individual tracks to keep forever.</p>
 			<p>The following code will add the Spotify repository, authenticate it (which makes sure the software you install is the official one), checks for the latest version, and installs Spotify. You should copy and paste this code into your Terminal and execute it.</p>
 			<textarea class="textleft code resize">sudo apt-add-repository -y &quot;deb http://repository.spotify.com stable non-free&quot; &&
