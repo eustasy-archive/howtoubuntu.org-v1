ecba28b5abac8912c7db2ca735189414e566c56c
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2014-02-09 20:57:20 +0000

    Moved to jsDelivr

--- header.php
+++ header.php
@@ -18,11 +18,12 @@
 	<link rel="canonical" href="<?php echo $Sitewide_Root.$Canonical; ?>">
 	<link rel="icon" href="<?php echo $Sitewide_Root; ?>favicon.ico">
 	<link rel="shortcut icon" href="<?php echo $Sitewide_Root; ?>favicon.ico">
-	<link rel="stylesheet" media="all" href="http://fonts.googleapis.com/css?family=Ubuntu+Mono|Ubuntu:300,300italic,400">
+	<link rel="stylesheet" media="all" href="//fonts.googleapis.com/css?family=Ubuntu+Mono|Ubuntu:300,300italic,400">
+	<link rel="stylesheet" media="screen" href="//cdn.jsdelivr.net/fancybox/2.1.5/jquery.fancybox.min.css">
 	<link rel="stylesheet" media="all" href="<?php echo $Sitewide_Root; ?>assets/css/reset.col.min.css">
 	<link rel="stylesheet" media="all" href="<?php echo $Sitewide_Root; ?>assets/css/htu.css">
 
-	<script async>
+	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -31,22 +32,18 @@
 		ga('send', 'pageview');
 	</script>
 
-	<script async src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js"></script>
-	<!--[if (lt IE 9) & (!IEMobile)]><script src="http://cdnjs.cloudflare.com/ajax/libs/selectivizr/1.0.2/selectivizr-min.js"></script><![endif]-->
-
 	<!-- jQuery -->
 	<script>var jQl={q:[],dq:[],gs:[],ready:function(a){'function'==typeof a&&jQl.q.push(a);return jQl},getScript:function(a,c){jQl.gs.push([a,c])},unq:function(){for(var a=0;a<jQl.q.length;a++)jQl.q[a]();jQl.q=[]},ungs:function(){for(var a=0;a<jQl.gs.length;a++)jQuery.getScript(jQl.gs[a][0],jQl.gs[a][1]);jQl.gs=[]},bId:null,boot:function(a){'undefined'==typeof window.jQuery.fn?jQl.bId||(jQl.bId=setInterval(function(){jQl.boot(a)},25)):(jQl.bId&&clearInterval(jQl.bId),jQl.bId=0,jQl.unqjQdep(),jQl.ungs(),jQuery(jQl.unq()), 'function'==typeof a&&a())},booted:function(){return 0===jQl.bId},loadjQ:function(a,c){setTimeout(function(){var b=document.createElement('script');b.src=a;document.getElementsByTagName('head')[0].appendChild(b)},1);jQl.boot(c)},loadjQdep:function(a){jQl.loadxhr(a,jQl.qdep)},qdep:function(a){a&&('undefined'!==typeof window.jQuery.fn&&!jQl.dq.length?jQl.rs(a):jQl.dq.push(a))},unqjQdep:function(){if('undefined'==typeof window.jQuery.fn)setTimeout(jQl.unqjQdep,50);else{for(var a=0;a<jQl.dq.length;a++)jQl.rs(jQl.dq[a]); jQl.dq=[]}},rs:function(a){var c=document.createElement('script');document.getElementsByTagName('head')[0].appendChild(c);c.text=a},loadxhr:function(a,c){var b;b=jQl.getxo();b.onreadystatechange=function(){4!=b.readyState||200!=b.status||c(b.responseText,a)};try{b.open('GET',a,!0),b.send('')}catch(d){}},getxo:function(){var a=!1;try{a=new XMLHttpRequest}catch(c){for(var b=['MSXML2.XMLHTTP.5.0','MSXML2.XMLHTTP.4.0','MSXML2.XMLHTTP.3.0','MSXML2.XMLHTTP','Microsoft.XMLHTTP'],d=0;d<b.length;++d){try{a= new ActiveXObject(b[d])}catch(e){continue}break}}finally{return a}}};if('undefined'==typeof window.jQuery){var $=jQl.ready,jQuery=$;$.getScript=jQl.getScript};</script>
 	<!--[if lt IE 9]>
-		<script>jQl.loadjQ('//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js');</script>
+		<script>jQl.loadjQ('//cdn.jsdelivr.net/g/modernizr,selectivizr,jquery@1.11.0,fancybox,jquery.autosize,jquery.equalize');</script>
 	<![endif]-->
 	<!--[if IE 9]><!-->
-		<script>jQl.loadjQ('//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js');</script>
+		<script>jQl.loadjQ('//cdn.jsdelivr.net/g/modernizr,jquery,fancybox,jquery.autosize,jquery.equalize');</script>
 	<!--<![endif]-->
 
 	<!-- Fancybox -->
 	<script>
-		jQl.loadjQdep('//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.pack.js');
-		jQl.loadjQdep('//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/helpers/jquery.fancybox-media.js');
+		jQl.loadjQdep('//cdn.jsdelivr.net/fancybox/2.1.5/helpers/jquery.fancybox-media.min.js');
 		$(function(){
 			$('.fancybox').fancybox();
 			$('.fancybox-media').fancybox({
@@ -61,24 +58,6 @@
 			});
 		});
 	</script>
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
-
-	<!-- Autosize Textarea -->
-	<script>
-		jQl.loadjQdep('//cdnjs.cloudflare.com/ajax/libs/autosize.js/1.18.1/jquery.autosize.min.js');
-		$(function(){
-			$('textarea.resize').autosize();
-		});
-	</script>
-
-	<!-- jQuery.equalize -->
-	<script>
-		jQl.loadjQdep('<?php echo $Sitewide_Root; ?>assets/js/jquery.equalize.min.js');
-		$(function(){
-			equalize();
-			window.onresize=function(){equalize()};
-		});
-	</script>
 
 	<!-- Code select-all functions -->
 	<script>
@@ -89,6 +68,12 @@
 			$('textarea.code').dblclick(function() { $(this).select(); });
 		});
 	</script>
+
+	<!-- Autosize Textarea -->
+	<script>$(function(){$('textarea.resize').autosize()})</script>
+
+	<!-- jQuery.equalize -->
+	<script>$(function(){equalize();window.onresize=function(){equalize()}});</script>
 
 </head>
 
