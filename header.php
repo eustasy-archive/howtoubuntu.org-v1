2ba55f1483643c63d83a996a813f5aad1276b62a
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2014-02-12 19:14:10 +0000

    Migrate from Fancybox to Colorbox

--- header.php
+++ header.php
@@ -19,10 +19,10 @@
 	<link rel="icon" href="<?php echo $Sitewide_Root; ?>favicon.ico">
 	<link rel="shortcut icon" href="<?php echo $Sitewide_Root; ?>favicon.ico">
 	<link rel="stylesheet" media="all" href="//fonts.googleapis.com/css?family=Ubuntu+Mono|Ubuntu:300,300italic,400">
-	<link rel="stylesheet" media="screen" href="//cdn.jsdelivr.net/fancybox/2.1.5/jquery.fancybox.min.css">
 	<link rel="stylesheet" media="all" href="<?php echo $Sitewide_Root; ?>assets/css/reset.col.min.css">
 	<link rel="stylesheet" media="all" href="<?php echo $Sitewide_Root; ?>assets/css/htu.css">
 
+	<!-- Google Analytics -->
 	<script>
 		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -34,28 +34,20 @@
 
 	<!-- jQuery -->
 	<script>var jQl={q:[],dq:[],gs:[],ready:function(a){'function'==typeof a&&jQl.q.push(a);return jQl},getScript:function(a,c){jQl.gs.push([a,c])},unq:function(){for(var a=0;a<jQl.q.length;a++)jQl.q[a]();jQl.q=[]},ungs:function(){for(var a=0;a<jQl.gs.length;a++)jQuery.getScript(jQl.gs[a][0],jQl.gs[a][1]);jQl.gs=[]},bId:null,boot:function(a){'undefined'==typeof window.jQuery.fn?jQl.bId||(jQl.bId=setInterval(function(){jQl.boot(a)},25)):(jQl.bId&&clearInterval(jQl.bId),jQl.bId=0,jQl.unqjQdep(),jQl.ungs(),jQuery(jQl.unq()), 'function'==typeof a&&a())},booted:function(){return 0===jQl.bId},loadjQ:function(a,c){setTimeout(function(){var b=document.createElement('script');b.src=a;document.getElementsByTagName('head')[0].appendChild(b)},1);jQl.boot(c)},loadjQdep:function(a){jQl.loadxhr(a,jQl.qdep)},qdep:function(a){a&&('undefined'!==typeof window.jQuery.fn&&!jQl.dq.length?jQl.rs(a):jQl.dq.push(a))},unqjQdep:function(){if('undefined'==typeof window.jQuery.fn)setTimeout(jQl.unqjQdep,50);else{for(var a=0;a<jQl.dq.length;a++)jQl.rs(jQl.dq[a]); jQl.dq=[]}},rs:function(a){var c=document.createElement('script');document.getElementsByTagName('head')[0].appendChild(c);c.text=a},loadxhr:function(a,c){var b;b=jQl.getxo();b.onreadystatechange=function(){4!=b.readyState||200!=b.status||c(b.responseText,a)};try{b.open('GET',a,!0),b.send('')}catch(d){}},getxo:function(){var a=!1;try{a=new XMLHttpRequest}catch(c){for(var b=['MSXML2.XMLHTTP.5.0','MSXML2.XMLHTTP.4.0','MSXML2.XMLHTTP.3.0','MSXML2.XMLHTTP','Microsoft.XMLHTTP'],d=0;d<b.length;++d){try{a= new ActiveXObject(b[d])}catch(e){continue}break}}finally{return a}}};if('undefined'==typeof window.jQuery){var $=jQl.ready,jQuery=$;$.getScript=jQl.getScript};</script>
+	<!-- If pre-9 IE, use legacy jQuery and add in Selectivizr -->
 	<!--[if lt IE 9]>
-		<script>jQl.loadjQ('//cdn.jsdelivr.net/g/modernizr,selectivizr,jquery@1.11.0,fancybox,jquery.autosize,jquery.equalize');</script>
+		<script>jQl.loadjQ('//cdn.jsdelivr.net/g/selectivizr,modernizr,jquery@1.11.0,colorbox,jquery.autosize,jquery.equalize');</script>
 	<![endif]-->
+	<!-- Else, just play it cool and load stuff -->
 	<!--[if IE 9]><!-->
-		<script>jQl.loadjQ('//cdn.jsdelivr.net/g/modernizr,jquery,fancybox,jquery.autosize,jquery.equalize');</script>
+		<script>jQl.loadjQ('//cdn.jsdelivr.net/g/modernizr,jquery,colorbox,jquery.autosize,jquery.equalize');</script>
 	<!--<![endif]-->
 
-	<!-- Fancybox -->
+	<!-- Colorbox -->
 	<script>
-		jQl.loadjQdep('//cdn.jsdelivr.net/fancybox/2.1.5/helpers/jquery.fancybox-media.min.js');
 		$(function(){
-			$('.fancybox').fancybox();
-			$('.fancybox-media').fancybox({
-				openEffect  : 'none',
-				closeEffect : 'none',
-				'padding'	: 0,
-				width		: 1072,
-				height		: 603,
-				helpers : {
-					media : {}
-				}
-			});
+			$('.fancybox, .colobox, .gallery').colorbox({rel:'gallery'});
+			$('.fancybox-media, .youtube').colorbox({ iframe: true, width: 1280, height: 720 });
 		});
 	</script>
 
@@ -70,10 +62,10 @@
 	</script>
 
 	<!-- Autosize Textarea -->
-	<script>$(function(){$('textarea.resize').autosize()})</script>
+	<script>$(function(){$('textarea.resize').autosize()});</script>
 
 	<!-- jQuery.equalize -->
-	<script>$(function(){equalize();window.onresize=function(){equalize()}});</script>
+	<script>$(function(){equalize();window.onresize=function(){equalize();}});</script>
 
 </head>
 
@@ -83,7 +75,7 @@
 
 	<header class="group">
 		<div class="col span_1_of_3">
-			<h1><a href="<?php echo $Request['scheme'].'://'.$Request['host']; ?>"><!--<img src=""> --><?php echo $Sitewide_Title; ?></a></h1>
+			<h1><a href="<?php echo $Sitewide_Root.'">'.$Sitewide_Title; ?></a></h1>
 		</div>
 		<div class="col span_2_of_3 search">
 			<script>
