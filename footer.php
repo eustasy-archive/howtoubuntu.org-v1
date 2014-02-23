6cf289227af1cdedaf6d85ddb022e2b060b32229
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2014-02-23 22:25:08 +0000

    Add Self-Served Social

--- footer.php
+++ footer.php
@@ -4,13 +4,55 @@
 	<div class="content bottom"></div>
 
 	<div class="section group content last">
-		<!-- TODO Trending -->
-		<!-- TODO Social: Order and Link
-			Google+		+HowtoUbuntuOrg
-			YouTube		HowtoUbuntuOrg
-			Twitter		htubuntu
-			Facebook	howtoubuntu
-			-->
+		<div class="col span_1_of_3">
+			<h3>Social</h3>
+			<div class="networks">
+				<div class="network google">
+					<div class="box">
+						<a class="type" target="_blank" href="https://plus.google.com/+HowtoubuntuOrg"><span>Google+</span></a>
+						<script type="text/javascript">document.write('<div class="g-follow" data-annotation="bubble" data-height="20" data-href="//plus.google.com/117279225305045556184" data-rel="publisher"></div>');</script>
+						<noscript><!-- TODO Fallback to Link --></noscript>
+					</div>
+				</div>
+				<div class="network facebook">
+					<div class="box">
+						<a class="type" target="_blank" href="http://www.facebook.com/howtoubuntu"><span>Facebook</span></a>
+						<script type="text/javascript">document.write('<div class="fb-like" data-href="http://www.facebook.com/howtoubuntu" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>');</script>
+						<noscript><iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2Fhowtoubuntu&amp;width&amp;layout=button_count&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=21&amp;appId=211598199034258" scrolling="no" frameborder="0" style="border:none;overflow:hidden;height:21px;" allowTransparency="true"></iframe></noscript>
+					</div>
+				</div>
+				<div class="network twitter">
+					<div class="box">
+						<a class="type" target="_blank" href="https://twitter.com/htubuntu"><span>Twitter</span></a>
+						<script type="text/javascript">document.write('<a href="https://twitter.com/htubuntu" class="twitter-follow-button" data-show-count="true">Follow @htubuntu</a>');</script>
+						<noscript><iframe allowtransparency="true" frameborder="0" scrolling="no" src="//platform.twitter.com/widgets/follow_button.html?screen_name=htubuntu&size=large" style="width:300px; height:20px;"></iframe></noscript>
+					</div>
+				</div>
+				<div class="network youtube">
+					<div class="box">
+						<a class="type" target="_blank" href="https://www.youtube.com/HowtoUbuntuOrg"><span>YouTube</span></a>
+						<script type="text/javascript">document.write('<div class="g-ytsubscribe" data-channel="HowtoubuntuOrg" data-layout="default" data-count="default"></div>');</script>
+						<noscript><iframe scrolling="no" src="https://www.youtube.com/subscribe_embed?usegapi=1&bsv=o&channel=HowtoUbuntuOrg&origin=http%3A%2F%2Fhowtoubuntu.org&gsrc=3p&ic=1&jsh=m%3B%2F_%2Fscs%2Fapps-static%2F_%2Fjs%2Fk%3Doz.gapi.en_GB.Rdkraw0TVFs.O%2Fm%3D__features__%2Fam%3DEQ%2Frt%3Dj%2Fd%3D1%2Frs%3DAItRSTMianVTDgGh090TyMLDabhESajIJQ#_methods=onPlusOne%2C_ready%2C_close%2C_open%2C_resizeMe%2C_renderstart%2Concircled%2Cdrefresh%2Cerefresh%2Conload&id=I2_1386506353008&parent=http%3A%2F%2Fhowtoubuntu.org&pfname=&rpctoken=36299024" data-gapiattached="true"></iframe></noscript>
+					</div>
+				</div>
+			</div>
+			<!-- Google+ and YouTube -->
+			<script type="text/javascript">(function(){var po=document.createElement('script');po.type='text/javascript';po.async=true;po.src='https://apis.google.com/js/platform.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(po,s);})();</script>
+			<!-- Twitter -->
+			<script type="text/javascript">!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','twitter-wjs');</script>
+			<!-- Facebook -->
+			<div id="fb-root"></div>
+			<script type="text/javascript">(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id)) return;js=d.createElement(s);js.id=id;js.src='http://connect.facebook.net/en_GB/all.js#xfbml=1&appId=211598199034258';fjs.parentNode.insertBefore(js,fjs);}(document,'script','facebook-jssdk'));</script>
+		</div>
+		<div class="col span_1_of_3">
+			<h3>Trending</h3>
+			<!-- TODO Trending -->
+			<p><a href="#">This is a trending article.</a></p>
+			<p><a href="#">This is a another trending article.</a></p>
+			<p><a href="#">This is a another trending article.</a></p>
+			<p><a href="#">This is a another trending article.</a></p>
+		</div>
+		<div class="col span_1_of_3">
 			<div class="ad-skyscraper">
 				<script src="//serve.adsi.st/javascript.php?AdsistMemberID=lewisgoddard&AdsistAdClass=Rectangle&AdsistAdSize=Large"></script>
 				<noscript><iframe src="//serve.adsi.st/iframe.php?AdsistMemberID=lewisgoddard&AdsistAdClass=Rectangle&AdsistAdSize=Large"></iframe></noscript>
@@ -18,6 +60,7 @@
 				<noscript><iframe src="//serve.adsi.st/iframe.php?AdsistMemberID=lewisgoddard&AdsistAdClass=Rectangle&AdsistAdSize=Large"></iframe></noscript>
 				<p class="about-ads">Advertisement <a href="http://eustasy.org/advertisements">Why are there ads?</a></p>
 			</div>
+		</div>
 		<div class="clear"></div>
 	</div>
 
