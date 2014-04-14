9df6da27a1ec756f5b2458652c274901af957fc0
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2014-04-14 14:36:05 +0100

    Updated GitPull

--- broadcast/gitpull.php
+++ broadcast/gitpull.php
@@ -14,10 +14,10 @@ if ( isset($_REQUEST['payload']) ) {
 
 	// git reset --hard HEAD && git pull
 
-	$GitPull = nl2br( exec('git reset --hard HEAD && git pull') ); // Capture the output.
+	$GitPull = nl2br( shell_exec('git reset --hard HEAD && git pull') ); // Capture the output.
 
 } else { // Or not.
-	$GitPull = nl2br( exec('git reset --hard HEAD') ); // Just tell us where we are
+	$GitPull = nl2br( shell_exec('git reset --hard HEAD') ); // Just tell us where we are
 }
 
 // That is all.
@@ -30,11 +30,10 @@ if ( isset($_REQUEST['payload']) ) {
 <link rel="icon" type="image/x-icon" href="http://www.eustasy.co.uk/favicon.ico">
 <link rel="shortcut icon" type="image/x-icon" href="http://www.eustasy.co.uk/favicon.ico">
 <link rel="apple shortcut icon" href="http://www.eustasy.co.uk/wp-content/themes/eustasy-three/200.png">
-<link href='http://fonts.googleapis.com/css?family=Raleway:400' rel='stylesheet' type='text/css'>
 <!--[if IE ]><style type="text/css">body { text-align: left !important; }</style><![endif]-->
 <!--[if IE 8]><style type="text/css">body { font-style: normal !important; }</style><![endif]-->
 <style type="text/css">
-body{background:#fafafa;margin:0;padding:0;text-align:center;color:#333333;font:400 1em/1.4 'Raleway',Sans;min-width:750px;}
+body{background:#fafafa;margin:0;padding:0;text-align:center;color:#333333;font:400 1em/1.4 'Open Sans',sans-serif;min-width:750px;}
 #small-container{margin:15% auto 12%;padding:5% 0 4%;width:100%;background:#ffffff;}
 </style>
 </head>
