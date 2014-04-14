6f0a9e7c4c3f778bfd75d01278df88b75b1491f9
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2014-04-14 14:25:14 +0100

    Add GitPull

--- broadcast/gitpull.php
+++ broadcast/gitpull.php
@@ -1,53 +1,46 @@
 <?php
 
-	$Title_HTML = 'GitPull';
-	$Title_Plain = $Title_HTML;
+// This is the GitPull file. The correct URL to active it is:
+// http://yourdomain.com/gitpull
 
-	$Description_HTML = 'GitPull for How to Ubuntu';
-	$Description_Plain = $Description_HTML;
+// If the auth is correct and Git posted a payload.
+// We can check for the payload to help stop attacks.
+// isset($_POST['payload'])
+if ( isset($_REQUEST['payload']) ) {
+	// We're going to run a few commands now.
+	// git reset --hard HEAD	// Cleans out uncommitted changes.
+	// git clean -f -d		// Remove junk files. DO NOT EXECUTE. DELETES DOWNLOADS.
+	// git pull			// Get the latest version.
 
-	$Keywords = 'git pull update contriute';
+	// git reset --hard HEAD && git pull
 
-	$Featured_Image = '';
+	$GitPull = nl2br( shell_exec('git reset --hard HEAD && git pull') ); // Capture the output.
 
-	$Canonical = 'gitpull';
-
-	$Post_Type = 'Function';
-	$Post_Category = '';
-
-	require_once __DIR__.'/../request.php';
-
-if ($Request['path'] === $Place['path'].$Canonical) {
-
-	require '../header.php';
-
-	echo '
-	<h2>GitPull</h2>';
-
-	// If the auth is correct and Git posted a payload.
-	// We can check for the payload to help stop attacks.
-	// isset($_POST['payload'])
-	// for some reason post doesn't seem to always get it.
-
-	if (isset($_REQUEST['payload'])) {
-		// We're going to run a few commands now.
-		// git reset --hard HEAD	// Cleans out uncommitted changes.
-		// git clean -f -d		// Remove junk files. DO NOT EXECUTE. DELETES DOWNLOADS.
-		// git pull			// Get the latest version.
-
-		// git reset --hard HEAD && git pull
-
-		$GitPull = nl2br( shell_exec('git reset --hard HEAD && git pull') ); // Capture the output.
-
-	} else { // Or not.
-		$GitPull = nl2br( shell_exec('git reset --hard HEAD') ); // Just tell us where we are
-	}
-
-	// That is all.
-
-	// Kind of
-	echo '
-<pre>'.$GitPull.'</pre>';
-
-	require '../footer.php';
+} else { // Or not.
+	$GitPull = nl2br( shell_exec('git reset --hard HEAD') ); // Just tell us where we are
 }
+
+// That is all.
+
+?><!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>GitPull</title>
+<link rel="icon" type="image/x-icon" href="http://www.eustasy.co.uk/favicon.ico">
+<link rel="shortcut icon" type="image/x-icon" href="http://www.eustasy.co.uk/favicon.ico">
+<link rel="apple shortcut icon" href="http://www.eustasy.co.uk/wp-content/themes/eustasy-three/200.png">
+<link href='http://fonts.googleapis.com/css?family=Raleway:400' rel='stylesheet' type='text/css'>
+<!--[if IE ]><style type="text/css">body { text-align: left !important; }</style><![endif]-->
+<!--[if IE 8]><style type="text/css">body { font-style: normal !important; }</style><![endif]-->
+<style type="text/css">
+body{background:#fafafa;margin:0;padding:0;text-align:center;color:#333333;font:400 1em/1.4 'Raleway',Sans;min-width:750px;}
+#small-container{margin:15% auto 12%;padding:5% 0 4%;width:100%;background:#ffffff;}
+</style>
+</head>
+<body>
+<div id="small-container">
+<pre><?php echo $GitPull; // Kind of. ?></pre>
+</div>
+</body>
+</html>
