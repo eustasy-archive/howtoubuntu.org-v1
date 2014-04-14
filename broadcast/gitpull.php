726a8dbc99aa0f678d0f6ede02bcb7362e9d6039
Lewis Goddard <lewisgoddard@eustasy.co.uk>
2014-04-14 14:51:33 +0100

    Updated GitPull

--- broadcast/gitpull.php
+++ broadcast/gitpull.php
@@ -1,45 +1,40 @@
-<?php
-
-// This is the GitPull file. The correct URL to active it is:
-// http://yourdomain.com/gitpull
-
-// If the auth is correct and Git posted a payload.
-// We can check for the payload to help stop attacks.
-// isset($_POST['payload'])
-if ( isset($_REQUEST['payload']) ) {
-	// We're going to run a few commands now.
-	// git reset --hard HEAD	// Cleans out uncommitted changes.
-	// git clean -f -d		// Remove junk files. DO NOT EXECUTE. DELETES DOWNLOADS.
-	// git pull			// Get the latest version.
-
-	// git reset --hard HEAD && git pull
-
-	$GitPull = nl2br( shell_exec('git reset --hard HEAD && git pull') ); // Capture the output.
-
-} else { // Or not.
-	$GitPull = nl2br( shell_exec('git reset --hard HEAD') ); // Just tell us where we are
-}
-
-// That is all.
-
-?><!DOCTYPE HTML>
+<!DOCTYPE HTML>
 <html>
-<head>
-<meta charset="UTF-8">
-<title>GitPull</title>
-<link rel="icon" type="image/x-icon" href="http://www.eustasy.co.uk/favicon.ico">
-<link rel="shortcut icon" type="image/x-icon" href="http://www.eustasy.co.uk/favicon.ico">
-<link rel="apple shortcut icon" href="http://www.eustasy.co.uk/wp-content/themes/eustasy-three/200.png">
-<!--[if IE ]><style type="text/css">body { text-align: left !important; }</style><![endif]-->
-<!--[if IE 8]><style type="text/css">body { font-style: normal !important; }</style><![endif]-->
-<style type="text/css">
-body{background:#fafafa;margin:0;padding:0;text-align:center;color:#333333;font:400 1em/1.4 'Open Sans',sans-serif;min-width:750px;}
-#small-container{margin:15% auto 12%;padding:5% 0 4%;width:100%;background:#ffffff;}
-</style>
-</head>
+	<head>
+		<meta charset="UTF-8">
+		<title>GitPull</title>
+		<link rel="icon" type="image/x-icon" href="http://www.eustasy.co.uk/favicon.ico">
+		<link rel="shortcut icon" type="image/x-icon" href="http://www.eustasy.co.uk/favicon.ico">
+		<link rel="apple shortcut icon" href="http://www.eustasy.co.uk/wp-content/themes/eustasy-three/200.png">
+		<!--[if IE ]><style type="text/css">body { text-align: left !important; }</style><![endif]-->
+		<!--[if IE 8]><style type="text/css">body { font-style: normal !important; }</style><![endif]-->
+		<style type="text/css">
+			body{background:#fafafa;margin:0;padding:0;text-align:center;color:#333333;font:400 1em/1.4 'Open Sans',sans-serif;min-width:750px;}
+			#small-container{margin:15% auto 12%;padding:5% 0 4%;width:100%;background:#ffffff;}
+		</style>
+	</head>
 <body>
-<div id="small-container">
-<pre><?php echo $GitPull; // Kind of. ?></pre>
-</div>
+	<div id="small-container">
+		<pre>
+			<?php
+				// If the auth is correct and Git posted a payload.
+				// We can check for the payload to help stop attacks.
+				// isset($_POST['payload'])
+				if ( isset($_REQUEST['payload']) ) {
+					// We're going to run a few commands now.
+					// git reset --hard HEAD	// Cleans out uncommitted changes.
+					// git clean -f -d		// Remove junk files. DO NOT EXECUTE. DELETES DOWNLOADS.
+					// git pull			// Get the latest version.
+
+					// git reset --hard HEAD && git pull
+
+					echo nl2br( shell_exec('git reset --hard HEAD && git pull') ); // Capture the output.
+
+				} else { // Or not.
+					echo nl2br( shell_exec('git reset --hard HEAD') ); // Just tell us where we are
+				}
+			?>
+		</pre>
+	</div>
 </body>
 </html>
