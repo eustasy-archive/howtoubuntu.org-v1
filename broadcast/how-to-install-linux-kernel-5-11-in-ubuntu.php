440a34fbeabdc3f958c519feaaf6b984f8b71430
Lewis Goddard <lewisgoddard@eustasy.org>
2021-07-16 12:41:55 +0100

    Update how-to-install-linux-kernel-5-11-in-ubuntu.php

--- broadcast/how-to-install-linux-kernel-5-11-in-ubuntu.php
+++ broadcast/how-to-install-linux-kernel-5-11-in-ubuntu.php
@@ -38,26 +38,29 @@ echo " " &&
 read -p "Press Enter to continue, or abort by pressing CTRL+C" nothing &&
 echo " " &&
 echo "Downloading Kernel 5.11 Packages" &&
-echo "4 Files, ~79 MB to Download" &&
+echo "5 Files, ~89 MB to Download" &&
 echo " " &&
 echo "Creating Kernel Directory in Home folder" &&
 echo " " &&
 mkdir -p $HOME/howtoubuntu-kernel-5-11 &&
 cd $HOME/howtoubuntu-kernel-5-11 &&
 echo " " &&
-echo "Downloading File 1 of 4, 11 MB" &&
+echo "Downloading File 1 of 5, 11 MB" &&
 echo " " &&
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.6/amd64/linux-headers-5.11.6-051106_5.11.6-051106.202103111435_all.deb &&
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.22/amd64/linux-headers-5.11.0-20_5.11.0-20.21+21.10.1_all.deb &&
 echo " " &&
-echo "Downloading File 2 of 4, 2.4 MB" &&
+echo "Downloading File 2 of 5, 2.5 MB" &&
 echo " " &&
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.6/amd64/linux-headers-5.11.6-051106-generic_5.11.6-051106.202103111435_amd64.deb &&
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.22/amd64/linux-headers-5.11.0-20-generic_5.11.0-20.21+21.10.1_amd64.deb &&
 echo " " &&
-echo "Downloading File 3 of 4, 11 MB" &&
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.6/amd64/linux-image-unsigned-5.11.6-051106-generic_5.11.6-051106.202103111435_amd64.deb &&
+echo "Downloading File 3 of 5, 11 MB" &&
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.22/amd64/linux-image-unsigned-5.11.0-20-generic_5.11.0-20.21+21.10.1_amd64.deb &&
 echo " " &&
-echo "Downloading File 4 of 4, 54 MB" &&
-wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.6/amd64/linux-modules-5.11.6-051106-generic_5.11.6-051106.202103111435_amd64.deb &&
+echo "Downloading File 4 of 5, 17 MB" &&
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.22/amd64/linux-modules-5.11.0-20-generic_5.11.0-20.21+21.10.1_amd64.deb &&
+echo " " &&
+echo "Downloading File 5 of 5, 47 MB" &&
+wget https://kernel.ubuntu.com/~kernel-ppa/mainline/v5.11.22/amd64/linux-modules-extra-5.11.0-20-generic_5.11.0-20.21+21.10.1_amd64.deb &&
 echo " " &&
 echo "Installing Kernel" &&
 echo "This step will require you password." &&
